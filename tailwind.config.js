599b6447c24c2f1266ae67501bfbdeb907a109fd
DostonbekZaripov <dastandev0110@gmail.com>
2024-11-23 00:18:54 +0500

    landing page tamomlandi

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        '1':'#F17E00',
+        '2':'#111111',
+        '3':'#888888',
+        '4':'#6F6F6F',
+      },
+    },
+  },
+  plugins: [],
+}
